ff92b08a87e542930cff1e2287b34caeaae3531b
Lawrence Lockhart <ldlockhart@gmail.com>
2019-03-09 19:14:25 -0600

    add home route

--- chatapp.py
+++ chatapp.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World</h1>'
 
+@app.route('/homt')
+def home():
+    return '<h3>Welcome to the ChatApp Home Page</h3>'
 if __name__ == "__main__":
     app.run(debug=True)
