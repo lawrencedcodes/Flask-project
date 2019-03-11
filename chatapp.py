be64ed4eb877e1edf2934aeab7245ad350f05b6d
Lawrence Lockhart <ldlockhart@gmail.com>
2019-03-10 19:20:27 -0500

    add template

--- chatapp.py
+++ chatapp.py
@@ -4,14 +4,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
-def index():
-    return '<h1>Hello World</h1>'
 
 @app.route('/home', methods=['GET','POST'])
 def home():
     chats = ['Hey guys hows it going','Fine dude, what are you up to?','Working on this Python App man, what else!']
     return render_template('group.html',user_1="*Johnny the Coder Guy*",chats=chats)
-
+@app.route('/index', methods=['GET','POST'])
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
